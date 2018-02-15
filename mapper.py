b2e39b234d1aab4626a755a60eeabbc4ecb48a08
LeonidKuznetsoVV <36491332+LeonidKuznetsoVV@users.noreply.github.com>
2018-02-15 07:18:57 +0000

    Delete mapper.py

--- mapper.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-
-for it,line in enumerate(sys.stdin):
-	#print ",".join((line.strip().split(",")[0],line))
-	print '{0}\t{1}'.format(line.strip().split(",")[0],line.strip())
