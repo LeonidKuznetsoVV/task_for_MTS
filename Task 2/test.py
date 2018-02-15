5f782afdfd251254eb0ac4a57eff768d18d811fd
LeonidKuznetsoVV <36491332+LeonidKuznetsoVV@users.noreply.github.com>
2018-02-15 09:43:19 +0000

    Тест на наличие строки

--- Task 2/test.py
+++ Task 2/test.py
@@ -1,17 +1,27 @@
 	
 
-import pydoop.hdfs as hdfs
+import pydoop.hdfs as hdfs 
+import logging
+logging.basicConfig(level = logging.DEBUG)
 
+# тест проверяет наличие строки в выходых файлах
+# для каждой строки ищется ее пара в директории с нужной датой
 
-for part in [1,2,3]:
-	with hdfs.open('/data/archive/2014-04-29/part-0000'+str(part)) as out_f:
-    		with open('file1.csv','r') as in_f:
-    			for out_line in out_f:
-				for in_line in in_f:
-        				a=set(out_line.strip().split(','))
+with open('file1.csv','r') as in_f:
+	for it,in_line in enumerate(in_f):
+		date=in_line.strip().split(',')[0]
+		for part in [1,2,3]:
+			with hdfs.open('/data/archive/'+date+'/part-0000'+str(part)) as out_f:
+				matching=[]
+				for out_line in out_f:
+					a=set(out_line.strip().split(','))
 					if a==set(in_line.strip().split(',')):
-						print True
+						matching.append(True) 
+						break
 					else:
-						print False
-						print a
-						print set(in_line.strip().split(','))
+						matching.append(False)
+			if any(matching):
+				matching=True
+				break
+        	if not matching:            
+			logging.debug("Error on line %s ,%s",it,in_line)
