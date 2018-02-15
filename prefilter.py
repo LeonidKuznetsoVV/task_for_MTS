ff2997ae7edd25baf9a29594217a53eeb7fdee65
LeonidKuznetsoVV <36491332+LeonidKuznetsoVV@users.noreply.github.com>
2018-02-15 07:16:35 +0000

    Update prefilter.py

--- prefilter.py
+++ prefilter.py
@@ -5,14 +5,14 @@ for line in sys.stdin: 
  	business_date,id,type,value=line.split(',')  
 	year,month,day=business_date.split('-')  
 	
-	check_statement=isinstance(id,int) \ 
-	and isinstance(value,int) \ 
-	and isinstance(year,int) \ 
-	and isinstance(month,int) \ 
-	and isinstance(day,int) \ 
-	and len(type)==3 \ 
-	and len(year)==4 \ 
-	and len(month)==2 \ 
+	check_statement=isinstance(id,int) \
+	and isinstance(value,int) \
+	and isinstance(year,int) \
+	and isinstance(month,int) \
+	and isinstance(day,int) \
+	and len(type)==3 \
+	and len(year)==4 \
+	and len(month)==2 \
 	and len(day)==2  
 	
 	if check_statement:  
